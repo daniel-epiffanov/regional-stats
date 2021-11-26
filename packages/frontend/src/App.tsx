d3a714da74dd1a46df3496980810b3a42476b4db
daniel-epiffanov <daniil.epifanov10@gmail.com>
2021-11-27 02:30:02 +0300

    update

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -5,7 +5,7 @@ import {
 	Route,
 	Routes,
 } from 'react-router-dom'
-import Home from './pages/Home'
+import Home from './pages/Home/Home'
 
 // const appMainHeader = 'Росстат'
 
--- packages/frontend/src/pages/Home/Home.tsx
+++ packages/frontend/src/pages/Home/Home.tsx
@@ -2,10 +2,10 @@ import ResponsiveBox, {
 	Row, Col, Item, Location,
 } from 'devextreme-react/responsive-box'
 import React from 'react'
-import ChartWrapper from '../components/Charts/ChartWrapper'
-import DoughnutChart from '../components/DoughnutChart'
-import MultipleAxesChart from '../components/MultipleAxesChart'
-import RangeSelector from '../components/RangeSelector'
+import ChartWrapper from '../../components/Charts/ChartWrapper'
+import DoughnutChart from '../../components/DoughnutChart'
+import MultipleAxesChart from '../../components/MultipleAxesChart'
+import RangeSelector from '../../components/RangeSelector'
 
 const Home = () => {
 	const [selectedRegion, setSelectedRegion] = React
@@ -25,35 +25,35 @@ const Home = () => {
 	return (
 		<ResponsiveBox>
 			<Row ratio={1} />
-			<Row ratio={2} />
-			<Row ratio={0.7} />
-			<Col ratio={0.5} screen="md lg" />
-			<Col ratio={2} />
-			<Col ratio={0.5} screen="md lg" />
+			<Row ratio={1} />
+			{/* <Row ratio={0.7} /> */}
+			<Col ratio={1} screen="md lg" />
+			{/* <Col ratio={2} />
+			<Col ratio={0.5} screen="md lg" /> */}
 
 			<Item>
 				<Location screen="md lg" row={0} col={0} />
-				<Location screen="xs sm" row={0} col={0} />
+				{/* <Location screen="xs sm" row={0} col={0} /> */}
 				<div className="content item">
-					<p>tree</p>
-				</div>
-			</Item>
-
-			<Item>
-				<Location screen="md lg" row={0} col={1} />
-				<Location screen="xs sm" row={1} col={0} />
-				<div className="left-side-bar item">
 					<DoughnutChart />
 				</div>
 			</Item>
 
 			<Item>
+				<Location screen="md lg" row={1} col={0} />
+				{/* <Location screen="xs sm" row={1} col={0} /> */}
+				<div className="left-side-bar item">
+					<p>map</p>
+				</div>
+			</Item>
+
+			{/* <Item>
 				<Location screen="md lg" row={0} col={2} />
 				<Location screen="xs sm" row={2} col={0} />
 				<div className="right-side-bar item">
 					<MultipleAxesChart />
 				</div>
-			</Item>
+			</Item> */}
 
 			{/* <Item>
 				<Location screen="md lg" row={0} col={1} />
