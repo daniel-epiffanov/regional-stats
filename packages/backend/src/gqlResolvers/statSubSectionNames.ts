7d0807a72fcaf07b47d23134aca12169126706df
daniel-epiffanov <daniil.epifanov10@gmail.com>
2022-04-14 04:52:19 +0300

    sub section names resolver is finally there

--- packages/backend/src/gqlResolvers/statSubSectionNames.ts
+++ packages/backend/src/gqlResolvers/statSubSectionNames.ts
@@ -1,36 +1,80 @@
 import { StatSubSectionNames } from '../../../../sharedTypes/gqlQueries'
 import statisticsModel from '../mongoModels/statistics'
 import { ResolverFnAsync } from './types/ResolverFn'
+import _ from 'lodash'
 
 type Args = Readonly<{
 	mainSectionName: string,
 }>
+
 
 const statisticsSubSectionNames: ResolverFnAsync<StatSubSectionNames> = async (
 	parent: any,
 	args: Args,
 ) => {
 	const { mainSectionName } = args
-	// const mongoRes = await statisticsModel.aggregate<{ subSections: StatSubSectionNames }>([
-	// 	{ $unwind: "$mainSections" },
-	// 	{ $match: { "mainSections.name": mainSectionName } },
-	// 	{ $project: { "mainSections.subSections.name": 1, "mainSections.subSections.children.name": 1 } },
-	// 	{ $project: { subSections: "$mainSections.subSections" } }
-	// ])
 
-	// fetching all top lvl sub Sections
-
-	const mongoRes = await statisticsModel.aggregate<{ subSections: StatSubSectionNames }>([
+	const mongoRes = await statisticsModel.aggregate<{ _id: StatSubSectionNames[0] }>([
 		{ $unwind: "$mainSections" },
 		{ $match: { "mainSections.name": mainSectionName } },
 		{ $project: { "mainSections.subSections.name": 1, "mainSections.subSections.children.name": 1 } },
 		{ $project: { subSections: "$mainSections.subSections" } },
 		{ $unwind: "$subSections" },
-		{ $group: { _id: "$subSections.name" } },
+		{ $group: { _id: { name: "$subSections.name", children: "$subSections.children" } } },
+		{ $sort: { _id: 1 } }
 	])
 
+	const rawSubSectionNames = mongoRes.map(({ _id }) => {
+		return _id
+	})
 
-	return mongoRes
+	let draftStatSubSectionNames: {
+		name: string,
+		children: {
+			name: string
+		}[] | null
+	}[] = []
+
+
+
+
+	for (let rawSubSectionNamesIndex = 0; rawSubSectionNamesIndex < rawSubSectionNames.length; rawSubSectionNamesIndex++) {
+		const rawSubSectionName = rawSubSectionNames[rawSubSectionNamesIndex];
+
+		const alreadyExist = !!draftStatSubSectionNames.find(rawStatSubSectionName => rawStatSubSectionName.name === rawSubSectionName.name)
+		if (alreadyExist) continue
+
+		if (!!rawSubSectionName.children) {
+			const childrenMongoRes = await statisticsModel.aggregate<{ _id: string }>([
+				{ $unwind: "$mainSections" },
+				{ $match: { "mainSections.name": mainSectionName } },
+				{ $project: { "mainSections.subSections.name": 1, "mainSections.subSections.children.name": 1 } },
+				{ $project: { subSections: "$mainSections.subSections" } },
+				{ $unwind: "$subSections" },
+				{ $match: { "subSections.name": rawSubSectionName.name } },
+				{ $unwind: "$subSections.children" },
+				{ $group: { _id: "$subSections.children.name" } },
+				{ $sort: { _id: 1 } }
+			])
+
+			draftStatSubSectionNames.push({
+				name: rawSubSectionName.name,
+				children: childrenMongoRes.length === 0 ? null : childrenMongoRes.map(({ _id: subSectionChildName }) => ({ name: subSectionChildName }))
+			})
+
+			continue
+		}
+
+		draftStatSubSectionNames.push({
+			name: rawSubSectionName.name,
+			children: null
+		})
+
+	}
+
+	const statSubSectionNames: StatSubSectionNames = [...draftStatSubSectionNames]
+
+	return statSubSectionNames
 }
 
 export default statisticsSubSectionNames
