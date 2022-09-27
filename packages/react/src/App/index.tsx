c67fadb9eb772cb846879b415202c8a5e5143a52
daniel-epiffanov <daniil.epifanov10@gmail.com>
2022-09-27 03:26:43 +0300

    no acces for low resolution devices

--- packages/react/src/App/index.tsx
+++ packages/react/src/App/index.tsx
@@ -1,3 +1,5 @@
+import { useWindowSize } from 'react-use';
+import { MAX_WINDOW_SIZE_DESKTOP } from '../config/settings';
 import { CategoriesMenuProvider } from '../context/CategoriesMenuContext';
 import CustomApolloProvider from '../context/CustomApolloProvider';
 import { RegionNamesProvider } from '../context/RegionNamesContext';
@@ -5,10 +7,19 @@ import setInitialTheme from '../helpers/setInitialTheme';
 import CategoriesMenu from './CategoriesMenu';
 import Header from './Header';
 import Main from './Main';
+import styles from './App.module.scss';
 
 setInitialTheme();
 
 const App = () => {
+  const {width: windowWidth} = useWindowSize();
+
+  if(windowWidth < MAX_WINDOW_SIZE_DESKTOP) return (
+    <div className={styles['no-access-root']}>
+      <p>Извините, но данное разрешение экрана пока не поддерживается.</p>
+    </div>
+  );
+
   return(
     <CustomApolloProvider>
       <Header />
--- packages/react/src/config/settings.ts
+++ packages/react/src/config/settings.ts
@@ -1,3 +1,5 @@
 export const ERROR_RELOAD_TIMEOUT_IN_SECONDS = 7;
 
 export const REGION_TYPE = 'region';
+
+export const MAX_WINDOW_SIZE_DESKTOP = 800;
