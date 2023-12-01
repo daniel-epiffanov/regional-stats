513274ad19e56dc872976b7240c15550ee788918
daniil epifanov <daniil.epifanov@nordclan.com>
2023-12-02 00:30:11 +0300

    host url

--- packages/backend/src/config/links.ts
+++ packages/backend/src/config/links.ts
@@ -1,5 +1,5 @@
 const isProd = process.env.NODE_ENV === 'production';
-const PROD_HOST = 'https://regional-stats.herokuapp.com';
+const PROD_HOST = 'https://regional-stats.onrender.com';
 const DEV_HOST = 'http://localhost:5000';
 
 export const HOST = isProd ? PROD_HOST : DEV_HOST;
